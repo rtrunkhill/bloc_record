7a8e7aa737806101b933d134a12681213b808949
Rory Trunkhill <rtrunkhill@gmail.com>
2018-02-24 19:42:43 +0000

    cp5 checkpoint work

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -28,6 +28,14 @@
         
         true
     end
+
+   def update_attribute(attribute, value)
+        self.class.update(self.id, { attribute => value })
+   end
+   
+    def update_attributes(updates)
+        self.class.update(self.id, updates)
+    end
  
    module ClassMethods
      def create(attrs)
@@ -44,6 +52,34 @@
        data["id"] = connection.execute("SELECT last_insert_rowid();")[0][0]
        new(data)
      end
+     
+    def update(ids, updates)
+        updates = BlocRecord::Utility.convert_keys(updates)
+        updates.delete "id"
+        updates_array = updates.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}" }
+        
+        if ids.class == Fixnum
+            where_clause = "WHERE id = #{ids};"
+        elsif ids.class == Array
+            where_clause = ids.empty? ? ";" : "WHERE id IN (#{ids.join(",")});"
+        else
+            where_clause = ";"
+        end
+        
+        connection.execute <<-SQL
+            UPDATE #{table}
+            SET #{updates_array * ","}
+            WHERE id = #{id};
+            SET #{updates_array * ","} #{where_clause}
+        SQL
+        
+        true
+    end
+    
+    def update_all(updates)
+        update(nil, updates)
+    end
+   
+   
    end
- 
  end
